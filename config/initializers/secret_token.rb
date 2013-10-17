6e0bc22502c8b29be5e37141ec2a290c5d403429
Clarence <clarenceboey@gmail.com>
2013-10-16 22:49:48 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Soxform::Application.config.secret_token = '3f986c7119d1a034b808b6e9683740dcb0ab3c4687d61c5d4ecf87640a382e115cd03c554af4f7bce729f9a6d04a2cc49203ef7879cfa80dd26763ac240b70b1'
